124a25c1b33562756420c58a6ea5e670d26439c8
Fadi Abji <fadi@wexer.se>
2024-09-09 15:22:55 +0200

    Add project files.

---
+++ WVVM.WPF/ModelViews/PersonViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using WVVM.WPF.Helpers;
+using WVVM.WPF.Models;
+
+
+namespace WVVM.WPF.ModelViews
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private Person _person;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public PersonViewModel()
+        {
+            _person = new Person { Name = "Fadi" };
+            ChangeNameCommand = new RelayCommand(ChangeName);
+        }
+
+        // Property for binding to the View
+        public string Name
+        {
+            get { return _person.Name; }
+            set
+            {
+                _person.Name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        // Command to update the name
+        public ICommand ChangeNameCommand { get; }
+
+        private void ChangeName()
+        {
+            Name = "changing Name to something else";
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
